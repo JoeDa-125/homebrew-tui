1296d40054787585e9d3fcaab670060e910d7c7e
Sam Tay <sam.chong.tay@gmail.com>
2020-02-17 00:37:52 -0800

    Add snake formula

---
+++ snake.rb
@@ -0,0 +1,16 @@
+class Snake < Formula
+  desc "Snake in your terminal"
+  homepage "https://github.com/samtay/snake"
+  url "https://github.com/samtay/snake/releases/download/0.1.0/snake-0.1.0-darwin.tar.gz"
+  sha256 "66de2a7a73f20374fb5100776e0a275026536f8acfca4ec527474950e6b0c267"
+
+  bottle :unneeded
+
+  def install
+    bin.install "snake"
+  end
+
+  test do
+    system "#{bin}/snake", "--help"
+  end
+end
