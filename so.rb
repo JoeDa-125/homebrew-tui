7c8b086b9f0647cedd76a3042b5931eb4eeb12e6
Sam Tay <sam.chong.tay@gmail.com>
2020-06-24 14:30:26 -0700

    Update repo link

--- so.rb
+++ so.rb
@@ -1,7 +1,7 @@
 class So < Formula
   desc "A terminal interface for StackExchange"
-  homepage "https://github.com/samtay/so"
-  url "https://github.com/samtay/so/releases/download/0.1.0/so-0.1.0-darwin.tar.gz"
+  homepage "https://github.com/samtay/so-hs"
+  url "https://github.com/samtay/so-hs/releases/download/0.1.0/so-0.1.0-darwin.tar.gz"
   sha256 "4dfb4294dcb5d8159e0c811e7812b34955ae7219f96fc46ddcdfbddcdda37a80"
 
   bottle :unneeded
