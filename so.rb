931b4bf51980dddcf2c131f3e61c95c06a6925ad
Sam Tay <sam.chong.tay@gmail.com>
2020-07-05 18:20:51 -0700

    Point so towards the Rust rewrite

--- so.rb
+++ so.rb
@@ -1,16 +1,21 @@
 class So < Formula
   desc "A terminal interface for StackOverflow"
   homepage "https://github.com/samtay/so"
-  url "https://github.com/samtay/so/releases/download/v0.3.3/so-v0.3.3-x86_64-apple-darwin.tar.gz"
-  sha256 "b9464b58a437065ac27f56700da6a78c5a127924e9f2d17ff874525395af0d5d"
+  url "https://github.com/samtay/so/archive/v0.3.6.tar.gz"
+  sha256 "6d2f541bb00dceb9163faf4cc44ff1bd39e07b46c35d6532e24b47d7ad6d47da"
 
-  bottle :unneeded
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
 
   def install
-    bin.install "so"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-    system "#{bin}/so", "--version"
+    system "#{bin}/so", "--help"
   end
 end
