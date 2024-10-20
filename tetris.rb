3af7dbc9a42735482fc6a2050380bcbdb17b7cc6
Sam Tay <samctay@pm.me>
2024-10-19 23:29:31 -0400

    Fix link

--- tetris.rb
+++ tetris.rb
@@ -1,7 +1,7 @@
 class Tetris < Formula
   desc "Tetris in your terminal"
   homepage "https://github.com/samtay/tetris"
-  url "https://github.com/samtay/tetris/releases/download/0.1.6/tetris-0.1.6-MacOS",
+  url "https://github.com/samtay/tetris/releases/download/v0.1.6/tetris-0.1.6-MacOS",
     :using => :nounzip
 
   sha256 "70f4a2cdff83f02c3220575faec3c499396ca42daad35276f618884c5961aebe"
