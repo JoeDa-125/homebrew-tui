bd7382923bc21a7ef021e35303cfa5b200a3fca7
Sam Tay <samctay@pm.me>
2024-10-19 23:02:36 -0400

    Update tetris formula

--- tetris.rb
+++ tetris.rb
@@ -1,8 +1,10 @@
 class Tetris < Formula
   desc "Tetris in your terminal"
   homepage "https://github.com/samtay/tetris"
-  url "https://github.com/samtay/tetris/releases/download/0.1.4/tetris-0.1.4-darwin.tar.gz"
-  sha256 "03dd9789062fa92db4bc86389ed03a2e7f00db33db48aca41c40c8f4f7baa3d0"
+  url "https://github.com/samtay/tetris/releases/download/0.1.6/tetris-0.1.6-MacOS",
+    :using => :nounzip
+
+  sha256 "70f4a2cdff83f02c3220575faec3c499396ca42daad35276f618884c5961aebe"
 
   def install
     bin.install "tetris"
