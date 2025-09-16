21d654d36d0680018367dc6ce9a32fb62053c05f
JoeDa-125 <jmd546@cornell.edu>
2025-09-16 00:29:20 -0400

    Update tetris.rb

--- tetris.rb
+++ tetris.rb
@@ -1,7 +1,7 @@
 class Tetris < Formula
   desc "Tetris in your terminal"
   homepage "https://github.com/JoeDa-125/tetris/tree/main"
-  url "https://github.com/JoeDa-125/tetris/releases/download/v0.1.6/tetris-0.1.6-MacOS",
+  url "https://github.com/JoeDa-125/tetris/releases/download/v0.1.6/tetris",
     :using => :nounzip
 
   sha256 "70f4a2cdff83f02c3220575faec3c499396ca42daad35276f618884c5961aebe"
